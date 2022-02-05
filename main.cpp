ec23dc713c523c6c7b8da9c57e401e7a72c883fb
Will <twitchifygamers@gmail.com>
2022-02-05 17:48:30 +0000

    Added switch statement for selection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+void CreateNewSurvey();
+void CompleteExistingSurvey();
+
 int main() {
     std::cout << "=======================" << std::endl;
     std::cout << "SURVEY APP" << std::endl;
@@ -9,6 +12,21 @@ int main() {
     std::cout << "What would you like to do?" << std::endl;
     std::cout << "[1] Create new survey" << std::endl;
     std::cout << "[2] Complete an existing survey" << std::endl;
+
+    int response;
+
+    switch (response) {
+        case 1:
+            CreateNewSurvey();
+            break;
+        case 2:
+            CompleteExistingSurvey();
+            break;
+        default:
+            std::cout << "Default is to create a new survey";
+            CreateNewSurvey();
+            break;
+    }
 
     return 0;
 }
@@ -19,6 +37,8 @@ void CreateNewSurvey() {
     std::cout << "What would you like to name the survey?" << std::endl;
 
     std::cin >> name;
+
+    std::ofstream Survey("./surveys/" + name + ".txt");
 }
 
 void CompleteExistingSurvey() {
